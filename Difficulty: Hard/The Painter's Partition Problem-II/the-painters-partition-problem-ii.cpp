3a74b7c5ef6124b78538c3cb023f913a7fb48f34
Ibrajchetry <ibrajchetry02@gmail.com>
2026-07-10 12:00:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+  public:
+    int minTime(vector<int>& arr, int k) {
+        // code here
+         if (k > arr.size())
+            return -1;
+
+        long long start = 0, end = 0, ans = -1;
+
+        for (int x : arr) {
+            start = max(start, (long long)x);
+            end += x;
+        }
+
+        while (start <= end) {
+
+            long long mid = start + (end - start) / 2;
+
+            long long pages = 0;
+            int count = 1;
+
+            for (int x : arr) {
+                pages += x;
+
+                if (pages > mid) {
+                    count++;
+                    pages = x;
+                }
+            }
+
+            if (count <= k) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return (int)ans;
+    }
+};
